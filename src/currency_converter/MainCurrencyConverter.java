268418f6fc76d6436afabda17ec8b4fea986e6d2
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-12-09 17:52:41 -0500

    [+] Conversor: Inicializa Proyecto

---
+++ src/currency_converter/MainCurrencyConverter.java
@@ -0,0 +1,9 @@
+package currency_converter;
+
+import javax.swing.*;
+
+public class MainCurrencyConverter {
+    public static void main(String[] args) {
+        System.out.println( "Conversor" );
+    }
+}
